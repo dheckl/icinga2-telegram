b7c54be462d6032e6d9e5e0417eb6758a76084c0
Daniel Heckl <daniel.heckl@gmail.com>
2018-07-29 15:10:25 +0200

    Use option arguments insted of env variables to be compatible with icingaweb2-director

--- telegram-host-notification.sh
+++ telegram-host-notification.sh
@@ -1,21 +1,65 @@
-#!/bin/sh
-if [ -n "$ICINGAWEB2_URL" ]; then
-    HOSTDISPLAYNAME="<a href=\"$ICINGAWEB2_URL/monitoring/host/show?host=$HOSTNAME\">$HOSTDISPLAYNAME</a>"
+#!/usr/bin/env bash
+
+while getopts 4:6::b:c:d:f:hi:l:n:o:r:s:t:v:w:x: opt
+do
+  case "$opt" in
+    4) HOSTADDRESS=$OPTARG ;;
+    6) HOSTADDRESS6=$OPTARG ;;
+    b) NOTIFICATIONAUTHORNAME=$OPTARG ;;
+    c) NOTIFICATIONCOMMENT=$OPTARG ;;
+    d) LONGDATETIME=$OPTARG ;; # required
+    f) MAILFROM=$OPTARG ;;
+    h) Help ;;
+    i) ICINGAWEB2URL=$OPTARG ;;
+    l) HOSTNAME=$OPTARG ;; # required
+    n) HOSTDISPLAYNAME=$OPTARG ;; # required
+    o) HOSTOUTPUT=$OPTARG ;; # required
+    r) USEREMAIL=$OPTARG ;; # required
+    s) HOSTSTATE=$OPTARG ;; # required
+    t) NOTIFICATIONTYPE=$OPTARG ;; # required
+    v) VERBOSE=$OPTARG ;;
+    w) TELEGRAM_BOT_TOKEN=$OPTARG ;; # required
+    x) TELEGRAM_CHAT_ID=$OPTARG ;; # required
+   \?) echo "ERROR: Invalid option -$OPTARG" >&2
+       Error ;;
+    :) echo "Missing option argument for -$OPTARG" >&2
+       Error ;;
+    *) echo "Unimplemented option: -$OPTARG" >&2
+       Error ;;
+  esac
+done
+
+if [ -n "$ICINGAWEB2URL" ]; then
+    HOSTDISPLAYNAME="<a href=\"$ICINGAWEB2URL/monitoring/host/show?host=$HOSTNAME\">$HOSTDISPLAYNAME</a>"
 fi
 template=$(cat <<TEMPLATE
-<strong>$NOTIFICATIONTYPE</strong> - $HOSTDISPLAYNAME is $HOSTSTATE
-
-Host: $HOSTALIAS
-Address: $HOSTADDRESS
-Date/Time: $LONGDATETIME
+<strong>$NOTIFICATIONTYPE</strong> $HOSTDISPLAYNAME is $HOSTSTATE!
 
 <pre>$HOSTOUTPUT</pre>
+
+When: $LONGDATETIME
+Host: $HOSTNAME
 TEMPLATE
 )
+
+## Check whether IPv4 was specified.
+if [ -n "$HOSTADDRESS" ] ; then
+  NOTIFICATION_MESSAGE="$NOTIFICATION_MESSAGE
+IPv4:	 $HOSTADDRESS"
+fi
+
+## Check whether IPv6 was specified.
+if [ -n "$HOSTADDRESS6" ] ; then
+  NOTIFICATION_MESSAGE="$NOTIFICATION_MESSAGE
+IPv6:	 $HOSTADDRESS6"
+fi
+
+## Check whether author and comment was specified.
 if [ -n "$NOTIFICATIONCOMMENT" ]; then
     template="$template
-Comment: ($NOTIFICATIONAUTHORNAME) $NOTIFICATIONCOMMENT
-"
+
+Comment by $NOTIFICATIONAUTHORNAME:
+  $NOTIFICATIONCOMMENT"
 fi
 
 /usr/bin/curl --silent --output /dev/null \
--- telegram-service-notification.sh
+++ telegram-service-notification.sh
@@ -1,23 +1,69 @@
-#!/bin/sh
-if [ -n "$ICINGAWEB2_URL" ]; then
-    HOSTDISPLAYNAME="<a href=\"$ICINGAWEB2_URL/monitoring/host/show?host=$HOSTNAME\">$HOSTDISPLAYNAME</a>"
-    SERVICEDISPLAYNAME="<a href=\"$ICINGAWEB2_URL/monitoring/service/show?host=$HOSTNAME&service=$SERVICEDESC\">$SERVICEDISPLAYNAME</a>"
+#!/usr/bin/env bash
+
+while getopts 4:6:b:c:d:e:f:hi:l:n:o:r:s:t:u:v:w:x: opt
+do
+  case "$opt" in
+    4) HOSTADDRESS=$OPTARG ;;
+    6) HOSTADDRESS6=$OPTARG ;;
+    b) NOTIFICATIONAUTHORNAME=$OPTARG ;;
+    c) NOTIFICATIONCOMMENT=$OPTARG ;;
+    d) LONGDATETIME=$OPTARG ;; # required
+    e) SERVICENAME=$OPTARG ;; # required
+    f) MAILFROM=$OPTARG ;;
+    h) Usage ;;
+    i) ICINGAWEB2URL=$OPTARG ;;
+    l) HOSTNAME=$OPTARG ;; # required
+    n) HOSTDISPLAYNAME=$OPTARG ;; # required
+    o) SERVICEOUTPUT=$OPTARG ;; # required
+    r) USEREMAIL=$OPTARG ;; # required
+    s) SERVICESTATE=$OPTARG ;; # required
+    t) NOTIFICATIONTYPE=$OPTARG ;; # required
+    u) SERVICEDISPLAYNAME=$OPTARG ;; # required
+    v) VERBOSE=$OPTARG ;;
+    w) TELEGRAM_BOT_TOKEN=$OPTARG ;; # required
+    x) TELEGRAM_CHAT_ID=$OPTARG ;; # required
+   \?) echo "ERROR: Invalid option -$OPTARG" >&2
+       Usage ;;
+    :) echo "Missing option argument for -$OPTARG" >&2
+       Usage ;;
+    *) echo "Unimplemented option: -$OPTARG" >&2
+       Usage ;;
+  esac
+done
+
+if [ -n "$ICINGAWEB2URL" ]; then
+    HOSTDISPLAYNAME="<a href=\"$ICINGAWEB2URL/monitoring/host/show?host=$HOSTNAME\">$HOSTDISPLAYNAME</a>"
+    SERVICEDISPLAYNAME="<a href=\"$ICINGAWEB2URL/monitoring/service/show?host=$HOSTNAME&service=$SERVICENAME\">$SERVICEDISPLAYNAME</a>"
 fi
 template=$(cat <<TEMPLATE
-<strong>$NOTIFICATIONTYPE</strong> $HOSTDISPLAYNAME - $SERVICEDISPLAYNAME is $SERVICESTATE
-
-Address: $HOSTADDRESS
-Date/Time: $LONGDATETIME
+<strong>$NOTIFICATIONTYPE</strong> $SERVICEDISPLAYNAME on $HOSTDISPLAYNAME is $SERVICESTATE
 
 <pre>$SERVICEOUTPUT</pre>
+
+When:    $LONGDATETIME
+Service: $SERVICENAME
+Host:    $HOSTNAME
 TEMPLATE
 )
 
+## Check whether IPv4 was specified.
+if [ -n "$HOSTADDRESS" ] ; then
+  template="$template
+IPv4:    $HOSTADDRESS"
+fi
+
+## Check whether IPv6 was specified.
+if [ -n "$HOSTADDRESS6" ] ; then
+  template="$template
+IPv6:    $HOSTADDRESS6"
+fi
+
+## Check whether author and comment was specified.
 if [ -n "$NOTIFICATIONCOMMENT" ]; then
   template="$template
 
-Comment: ($NOTIFICATIONAUTHORNAME) $NOTIFICATIONCOMMENT
-"
+Comment by $NOTIFICATIONAUTHORNAME:
+  $NOTIFICATIONCOMMENT"
 fi
 
 /usr/bin/curl --silent --output /dev/null \
